2af69d5837ff1d16d99f2683f49a182c1dfdb410
Jaskiran Kaur <jkaur372@my.centennialcollege.ca>
2020-04-22 23:10:30 -0400

    Adding the blogs app

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Project
 # Create your views here.
+
+def home(request):
+    projects=Project.objects.all()
+    #take all the project objects and pass to html
+    return render(request,'portfolio/home.html',{'projects':projects})
